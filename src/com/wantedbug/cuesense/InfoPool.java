f8e766400ad0033eae6a85bea2ff9008de98509a
Vikas Prabhu <wantedbug@gmail.com>
2014-07-14 15:03:41 +0300

    InfoPool thread-safety update + functions
    Apparently ArrayList is not thread-safe, so switching to Vector. Also
    added initial size to the list creation and some add/delete functions.

--- src/com/wantedbug/cuesense/InfoPool.java
+++ src/com/wantedbug/cuesense/InfoPool.java
@@ -5,6 +5,7 @@
 package com.wantedbug.cuesense;
 
 import java.util.ArrayList;
+import java.util.Vector;
 
 import com.wantedbug.cuesense.MainActivity.InfoType;
 
@@ -20,13 +21,27 @@ public class InfoPool {
 	private static final String TAG = "InfoPool";
 	
 	/**
+	 * Constants
+	 */
+	private static final int INIT_SIZE = 50;
+	
+	/**
 	 * Members
 	 */
 	// Static singleton instance
 	public static final InfoPool INSTANCE = new InfoPool();
 	
+	/**
+	 * This class represents an item in the InfoPool
+	 * @author vikasprabhu
+	 */
 	public class InfoItem {
+		/**
+		 * Members
+		 */
+		// The type of information
 		private InfoType mType;
+		// The information
 		private String mData;
 		
 		public InfoItem(InfoType type, String data) {
@@ -34,6 +49,9 @@ public class InfoPool {
 			mData = data;
 		}
 		
+		/**
+		 * Get-set methods 
+		 */
 		public InfoType type() { return mType; }
 		public void setType(InfoType type) { mType = type; }
 		public String data() { return mData; }
@@ -41,7 +59,7 @@ public class InfoPool {
 	}
 	
 	// List for the data
-	private ArrayList<InfoItem> mList = new ArrayList<InfoItem>();
+	private Vector<InfoItem> mList = new Vector<InfoItem>(INIT_SIZE);
 	
 	/**
 	 * Private c'tor to defeat instantiation
@@ -56,5 +74,35 @@ public class InfoPool {
 	public void add(InfoItem item) {
 		Log.d(TAG, "add: " + item.type() + item.data());
 		mList.add(item);
+	}
+	
+	/**
+	 * Clears the InfoPool
+	 */
+	public void clear() {
+		Log.d(TAG, "clear(): " + mList.size());
+		mList.clear();
+	}
+	
+	/**
+	 * Adds a list of InfoItems to the InfoPool
+	 * @param items
+	 */
+	public void addItems(ArrayList<InfoItem> items) {
+		Log.d(TAG, "addItems(): " + items.size());
+		mList.addAll(items);
+	}
+	
+	/**
+	 * Deletes all entries of a particular InfoType
+	 * @param type
+	 */
+	public void deleteType(InfoType type) {
+		Log.d(TAG, "deleteType(): " + type.toString());
+		for(InfoItem item : mList) {
+			if(item.type() == type) {
+				
+			}
+		}
 	}
 }
