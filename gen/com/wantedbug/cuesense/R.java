5bc54d4cd8f8d056612a97b67b7c0411fbc2f99a
Vikas Prabhu <wantedbug@gmail.com>
2014-07-20 16:29:35 +0300

    menu removed from Settings activity

--- gen/com/wantedbug/cuesense/R.java
+++ gen/com/wantedbug/cuesense/R.java
@@ -299,7 +299,6 @@ containing a value of this type.
     }
     public static final class menu {
         public static final int main=0x7f090000;
-        public static final int settings=0x7f090001;
     }
     public static final class string {
         public static final int action_settings=0x7f07001f;
--- src/com/wantedbug/cuesense/SettingsActivity.java
+++ src/com/wantedbug/cuesense/SettingsActivity.java
@@ -42,25 +42,6 @@ public class SettingsActivity extends FragmentActivity {
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.settings, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-	
-	@Override
 	protected void onPause() {
 		Log.i(TAG, "onPause()");
 		super.onPause();
