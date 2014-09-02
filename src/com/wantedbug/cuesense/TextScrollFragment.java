315b81b682c20a646ce1e6574663dbdc446d5c04
Vikas Prabhu <wantedbug@gmail.com>
2014-09-02 11:28:23 +0300

    different animations for different InfoTypes
    Also added Twitter logo and 3 text sizes for scrolling text

--- src/com/wantedbug/cuesense/TextScrollFragment.java
+++ src/com/wantedbug/cuesense/TextScrollFragment.java
@@ -14,6 +14,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.Animation.AnimationListener;
 import android.view.animation.AnimationUtils;
@@ -42,11 +43,12 @@ public class TextScrollFragment extends DialogFragment {
 	private TextView mContextHeader;
 	private TextView mScrollText;
 	// Animations for mScrollText
-	Animation mIntroAnim;
-	Animation mOutroAnim;
+	Animation mSlideInLeftAnim;
+	Animation mSlideOutRightAnim;
+	Animation mBlinkingAnim;
 	// Animation listeners to control mScrollText's animation and to get
 	// new item at the end of the animation
-	AnimationListener mIntroAnimListener = new AnimationListener() {
+	AnimationListener mSlideInLeftAnimListener = new AnimationListener() {
 		@Override
 		public void onAnimationStart(Animation animation) {
 			// Do nothing
@@ -54,7 +56,7 @@ public class TextScrollFragment extends DialogFragment {
 
 		@Override
 		public void onAnimationEnd(Animation animation) {
-			mScrollText.startAnimation(mOutroAnim);
+			mScrollText.startAnimation(mSlideOutRightAnim);
 		}
 
 		@Override
@@ -63,7 +65,7 @@ public class TextScrollFragment extends DialogFragment {
 		}
 	};
 	
-	AnimationListener mOutroAnimListener = new AnimationListener() {
+	AnimationListener mSlideOutRightAnimListener = new AnimationListener() {
 		@Override
 		public void onAnimationStart(Animation animation) {
 			// Do nothing
@@ -77,6 +79,23 @@ public class TextScrollFragment extends DialogFragment {
 		@Override
 		public void onAnimationEnd(Animation animation) {
 			getNextText();
+		}
+	};
+	
+	AnimationListener mBlinkingAnimListener = new AnimationListener() {
+		@Override
+		public void onAnimationStart(Animation animation) {
+			// Do nothing
+		}
+
+		@Override
+		public void onAnimationEnd(Animation animation) {
+			getNextText();
+		}
+
+		@Override
+		public void onAnimationRepeat(Animation animation) {
+			// Do nothing
 		}
 	};
 	
@@ -97,13 +116,21 @@ public class TextScrollFragment extends DialogFragment {
         mContextHeader = (TextView) dialogView.findViewById(R.id.contextHeader1);
         mScrollText = (TextView) dialogView.findViewById(R.id.scrollText1);
         
-        // Set scrolling text's intro and outro animations
-        mIntroAnim = AnimationUtils.loadAnimation(getActivity(), android.R.anim.slide_in_left);
-        mIntroAnim.setDuration(PUSH_INTERVAL_MS / 2);
-        mIntroAnim.setAnimationListener(mIntroAnimListener);
-        mOutroAnim = AnimationUtils.loadAnimation(getActivity(), android.R.anim.slide_out_right);
-        mOutroAnim.setDuration(PUSH_INTERVAL_MS / 2);
-        mOutroAnim.setAnimationListener(mOutroAnimListener);
+        /** Set up animations */
+        // Slide in from left
+        mSlideInLeftAnim = AnimationUtils.loadAnimation(getActivity(), android.R.anim.slide_in_left);
+        mSlideInLeftAnim.setDuration(PUSH_INTERVAL_MS / 2);
+        mSlideInLeftAnim.setAnimationListener(mSlideInLeftAnimListener);
+        // Slide out to right
+        mSlideOutRightAnim = AnimationUtils.loadAnimation(getActivity(), android.R.anim.slide_out_right);
+        mSlideOutRightAnim.setDuration(PUSH_INTERVAL_MS / 2);
+        mSlideOutRightAnim.setAnimationListener(mSlideOutRightAnimListener);
+        // Blink 4 times
+        mBlinkingAnim = new AlphaAnimation(0, 1);
+        mBlinkingAnim.setDuration(PUSH_INTERVAL_MS / 8);
+        mBlinkingAnim.setRepeatMode(Animation.REVERSE);
+        mBlinkingAnim.setRepeatCount(4);
+        mBlinkingAnim.setAnimationListener(mBlinkingAnimListener);
         
         return builder.create();
 	}
@@ -136,39 +163,48 @@ public class TextScrollFragment extends DialogFragment {
 		CueItem item = InfoPool.INSTANCE.getNext();
 		
 		// Set text size
-		if(item.data().length() <= 35) {
+		if(item.data().length() <= 25) {
+			Log.d(TAG, "font size large");
 			mScrollText.setTextSize(getResources().getDimension(R.dimen.scroll_text_size_large));
 		} else {
+			Log.d(TAG, "font size medium");
 			mScrollText.setTextSize(getResources().getDimension(R.dimen.scroll_text_size_medium));
 		}
 		
-		// Set other properties
-		if(mScrollText != null) {
-			switch(item.type()) {
-			case INFO_CUESENSE:
-				mImage.setImageDrawable(getResources().getDrawable(R.drawable.ic_launcher));
-				mImage.setBackgroundColor(0x00000000);
-				mContextHeader.setText(spacer + "What I did last summer..");
-				mScrollText.setTextColor(Color.parseColor("#FFA500")); // CueSense orange
-				break;
-			case INFO_FACEBOOK:
-				mImage.setImageDrawable(getResources().getDrawable(R.drawable.com_facebook_inverse_icon));
-				mImage.setBackgroundColor(getResources().getColor(R.color.com_facebook_blue));
-				mContextHeader.setText(spacer + "I like..");
-				mScrollText.setTextColor(Color.parseColor("#627AAD")); // Facebook blue
-				break;
-			case INFO_TWITTER:
-				mContextHeader.setText(spacer + "Twitter..");
-				mImage.setBackgroundColor(0x00000000);
-				mScrollText.setTextColor(Color.parseColor("#1dcaff")); // Twitter cyan
-				break;
-			default:
-				break;
-			}
+		// Set text properties and animation
+		switch(item.type()) {
+		case INFO_CUESENSE:
+			mImage.setImageDrawable(getResources().getDrawable(R.drawable.ic_launcher));
+			mImage.setBackgroundColor(0x00000000);
+			mContextHeader.setText(spacer + "What I did last summer..");
+			mScrollText.setTextColor(Color.parseColor("#FFA500")); // CueSense orange
+			mScrollText.setText(item.data());
+			mScrollText.startAnimation(mBlinkingAnim);
+			break;
+		case INFO_FACEBOOK:
+			mImage.setImageDrawable(getResources().getDrawable(R.drawable.com_facebook_inverse_icon));
+			mImage.setBackgroundColor(getResources().getColor(R.color.com_facebook_blue));
+			mContextHeader.setText(spacer + "I like..");
+			mScrollText.setTextColor(Color.parseColor("#627AAD")); // Facebook blue
+			mScrollText.setText(item.data());
+			mScrollText.startAnimation(mSlideInLeftAnim);
+			break;
+		case INFO_TWITTER:
+			mImage.setImageDrawable(getResources().getDrawable(R.drawable.twitter_logo_blue));
+			mImage.setBackgroundColor(0x00000000);
+			mContextHeader.setText(spacer + "On Twitter..");
+			mScrollText.setTextColor(Color.parseColor("#1dcaff")); // Twitter cyan
+			mScrollText.setText(item.data());
+			mScrollText.startAnimation(mSlideInLeftAnim);
+			break;
+		default:
+			mImage.setImageDrawable(getResources().getDrawable(R.drawable.ic_launcher));
+			mImage.setBackgroundColor(0x00000000);
+			mContextHeader.setText(spacer + "CueSense");
+			mScrollText.setTextColor(Color.parseColor("#FFA500")); // CueSense orange
+			mScrollText.setText("Hello!");
+			mScrollText.startAnimation(mBlinkingAnim);
+			break;
 		}
-		
-		// Set text and start appropriate animation
-		mScrollText.setText(item.data());
-		mScrollText.startAnimation(mIntroAnim);
 	}
 }
